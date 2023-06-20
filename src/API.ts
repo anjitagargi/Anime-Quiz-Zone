5cf8828b045732df84ad9e60cf89a22f484f4b74
anjitagargi <anjitagargi@gmail.com>
2023-06-21 00:41:54 +0530

    first commit

---
+++ src/API.ts
@@ -0,0 +1,36 @@
+import { shuffleArray } from "./utils";
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+// enum is a custom type
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export type QuestionState = Question & { answers: string[] };
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple&category=31`;
+    // await is used to wait for the fetch to complete
+    const data = await (await fetch(endpoint)).json();
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([
+                ...question.incorrect_answers, 
+                question.correct_answer,
+            ]),
+
+    }
+    ));
+};
+
---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export const shuffleArray = (array: any[]) =>
+ [...array].sort(() => Math.random() - 0.5);
+
+
+
+ 
